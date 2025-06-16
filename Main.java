5e23d774921d5b45151131dd6713207b15c04322
Genichi Maruo <gen1010f8e7@gmail.com>
2025-06-16 10:07:25 +0900

    間違った編集

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
     System.out.println("SEMonster!");
     Monster monster = new Monster();
     System.out.println(monster.toString());
+    System.out.println("召喚したモンスターは" + monster.name + "です。レア度は" + monster.rare + "です。");
   }
 }
